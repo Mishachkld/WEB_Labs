fde62d2b7da9e1e6f19557075937df16d21adbda
Mishach <mishachkld@gmail.com>
2024-04-20 20:56:56 +0200

    add save.php

---
+++ Lab4/code/back/save.php
@@ -0,0 +1,42 @@
+<?php
+require_once '../index.php';
+function redirectHome(): void
+{
+    header("Location: /");
+    exit();
+}
+
+if (false === isset($_POST['email'], $_POST['category'], $_POST['title'], $_POST['description'])) {
+    redirectHome();
+}
+$service = getService("Web", __DIR__ . '/../credentials/web-programming-credentials.json');
+$category = $_POST['category'];
+$email = $_POST['email'];
+$title = $_POST['title'];
+$description = $_POST['description'];
+
+$range = "Лист1";
+$dataRow =[
+    [
+        $category,
+        $email,
+        $title,
+        $description
+    ]
+];
+
+$spreadSheetId = "134eQmwAHkqQhXO_HqJLZLvfRmxy1vcPaudAtGdbdUnA";
+$body = new Google_Service_Sheets_ValueRange();
+$body->setValues($dataRow);
+$insert = [
+    'insertDataOption' => "INSERT_ROWS"
+];
+$params = [
+    'valueInputOption' => 'RAW'
+];
+$service->spreadsheets_values->append(
+    $spreadSheetId,
+    $range,
+    $body,
+    $params);
+redirectHome();
