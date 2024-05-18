c6a798d48c92642e6d287c947877c89f6715ea72
Mishach <mishachkld@gmail.com>
2024-05-18 16:08:18 +0200

    Lab 5. show the work of the class

--- Lab5/code/index.php
+++ Lab5/code/index.php
@@ -1,24 +1,9 @@
 <?php
+require 'WorkWithDB.php';
 // Сделать хранение объявлений в базе данных. Добавлять в базу и читать с базы.
-$hostName = 'db';
-$username = 'root';
-$password = 'password';
-$dbName = 'web';
-$mysql = new mysqli($hostName, $username, $password, $dbName);
-
-if (mysqli_connect_errno()) {
-    echo "Connect to server MySql unavailable. Code error: " . mysqli_connect_error();
-    exit("501");
-}
 $addDataQuery = 'INSERT INTO ad (email, title, description, category) VALUES("mishach@amogus.com", "smthing", "desc", "helicopter")';
-$selectAllFromAD = 'SELECT * FROM ad ORDER BY created DESC ';
-
-$mysql->query($addDataQuery);
-
-if ($result = $mysql->query($selectAllFromAD)) {
-    while ($row = $result->fetch_assoc()) {
-        echo $row['email'] . " : " . $row['title'];
-    }
-    $result->close();
-}
+$selectAllFromAD = 'SELECT * FROM ad ORDER BY created DESC';
+$mysql = WorkWithDataBase::getConnectToDataBase();
+WorkWithDataBase::addElementInDataBase($mysql, "fdfsdf@gmail.com", "fdsfsdfdsfs", "какое-то описангие");
+WorkWithDataBase::showAllElementsFormDataBase($mysql);
 $mysql->close();
